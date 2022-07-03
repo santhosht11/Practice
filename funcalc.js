ff291bd2225d8dfd4a3703467adfd93e23636b1f
bharath.k@newage-global.com <bharath.k@newage-global.com>
2022-07-03 21:44:11 +0530

    funcalc.js

---
+++ funcalc.js
@@ -0,0 +1,24 @@
+function add (x,y)
+{
+    var z;
+    z=x+y;
+    alert("the addition is"+z);
+}
+function sub (x,y)
+{
+    var z;
+    z=x-y;
+    alert("the substration is"+z);
+}
+function mult (x,y)
+{
+    var z;
+    z=x*y;
+    alert("the multplication is"+z);
+}
+function div (x,y)
+{
+    var z;
+    z=x/y;
+    alert("the division is"+z);
+}
